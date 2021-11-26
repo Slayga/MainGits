f0c1e2fb1e503e988000f5fa330d7b361a0e7e3c
Slayga <63261321+Slayga@users.noreply.github.com>
2021-11-26 21:20:00 +0100

    cleaning imports, disabled snake apperance,

--- snake3.1/snakeModule.py
+++ snake3.1/snakeModule.py
@@ -4,8 +4,7 @@ Date: 22-11-2021
 Info:
 Snake Module
 """
-from tkinter import Tk, Label, Grid
-from typing import Callable
+from tkinter import Tk, Label, N, S, W, E
 
 class Snake:
     def __init__(self, window: Tk, apperance: str, head_color: str, tail_color: str, start_pos:int, audio:bool=False):
@@ -15,21 +14,16 @@ class Snake:
         self.tail_color = tail_color
         self.start_pos = start_pos
 
-        
         self.alive = True
         self.direction = str()
         self.old_direction = str()
         
         self.audio = audio
         
-        self.lbl_snake = Label(self.window, font=(30),
-                                  text=self.apperance,
-                                  bg=self.head_color)
-        # Grid.rowconfigure(self.window, self.start_pos, weight=1)
-        # Grid.columnconfigure(self.window, self.start_pos, weight=1)
-        # self.window.grid_columnconfigure(0, weight=1)
+        self.lbl_snake = Label(self.window, bg=self.head_color)
+        
         self.lbl_snake.grid(column=self.start_pos,
-                            row=self.start_pos)
+                            row=self.start_pos, sticky=N + S + W+ E)
         
     def opposite_dir(self)->str:
         match self.direction:
@@ -43,4 +37,4 @@ class Snake:
                 return "Up"
     
     def change_direction(self, value=None):
-        print(value)
+        ...
