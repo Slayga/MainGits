cf8e91339ad0c0b997ad54152bc534ba6417307e
Slayga <63261321+Slayga@users.noreply.github.com>
2022-02-14 08:31:56 +0100

    ...

--- debug.py
+++ debug.py
@@ -3,3 +3,8 @@ Name: Gabriel Engberg
 Date: 04-10-2021
 Info: Debugging file, code gets removed and replaced often..nothing consistent
 """
+
+with open("text.tx", "r") as f:
+    for line in f.readlines():
+        print(line.replace("\n", "") if line.endswith("\n") else line)
+        # print(line)
---
+++ gabrielengberg_filhantering.py
@@ -0,0 +1,16 @@
+"""
+Name: Gabriel Engberg
+Date: 07-02-2022
+Info:
+<Insert information about file>
+"""
+
+
+def main():
+    print("Telefonlista".upper())
+    with open("tele.txt") as f:
+        for line in f:
+
+
+if __name__ == "__main__":
+    main()
